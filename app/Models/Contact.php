8696ea0b977f71a62feb3511ebc4fc28eb4d9135
paresiqbal <thecrowdel@gmail.com>
2024-06-14 21:58:03 +0700

    refactor: Update Contact model with table configuration

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory;
+    protected $primaryKey = 'id';
+    protected $KeyType = "int";
+    protected $table = 'contacts';
+    public $incrementing = true;
+    public $timestamps = true;
 }
